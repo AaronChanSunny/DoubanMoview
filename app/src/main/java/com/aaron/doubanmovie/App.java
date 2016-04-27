12848453112db0202c28f636154acc203cc35f7b
AaronChanSunny <aaronchansunny@gmail.com>
2016-04-28 02:08:57 +0800

    add Fab animation in InFragment.

--- app/src/main/java/com/aaron/doubanmovie/App.java
+++ app/src/main/java/com/aaron/doubanmovie/App.java
@@ -2,17 +2,8 @@ package com.aaron.doubanmovie;
 
 import android.app.Application;
 
-import butterknife.ButterKnife;
-
 /**
  * Created by aaronchan on 16/4/27.
  */
 public class App extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        ButterKnife.setDebug(true);
-    }
 }
